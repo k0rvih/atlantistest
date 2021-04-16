f0d4943418b3f2f5647ed29268b76c8153ba8b1b
Oleksandr Barbul <Oleksandr_Barbul@epam.com>
2021-04-16 18:03:51 +0300

    update name

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
 
   tags = {
-    Name        = atlantis-test
+    Name        = "atlantis-test"
     Description = "Managed by Terraform"
   }
 }
